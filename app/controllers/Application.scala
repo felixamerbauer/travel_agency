32da9b50c38a5f934c2b3fe13d124f0fad24fe5e
Felix Amerbauer <felix@amerbauer.at>
2013-11-27 00:57:30 +0100

    started implementing GUI

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,6 +4,13 @@ import play.api.Play.current
 import play.api.cache.Cached
 import play.api.mvc.Action
 import play.api.mvc.Controller
+import views.formdata.SearchFormData
+import play.api.data.Form
+import play.api.Logger.info
+import play.api.data._
+import play.api.data.Forms._
+import play.api.mvc.Action
+import play.api.mvc.Controller
 
 object Application extends Controller {
 
@@ -12,9 +19,25 @@ object Application extends Controller {
       Ok(views.html.index())
     }
   }
+  private val searchForm = Form(
+    mapping(
+      "from" -> list(text),
+      "to" -> list(text),
+      "start" -> text,
+      "end" -> text,
+      "travellers" -> list(text))(SearchFormData.apply)(SearchFormData.unapply))
+  private val travellers = Map("1" -> false, "2" -> true, "3" -> false, "4" -> false, "5" -> false)
+  private val from = Map("Mailand" -> true, "Madrid" -> false)
+  private val to = Map("New York" -> true, "Johannesburg" -> false)
 
   def travelSearch = Action {
-    Ok(views.html.travelsearch())
+    val data = SearchFormData()
+    Ok(views.html.travelsearch(searchForm, from, to, travellers))
+  }
+
+  def travelSearchPost = Action {
+    val data = SearchFormData()
+    Ok(views.html.travelsearch(searchForm, from, to, travellers))
   }
 
   def travelList = Action {
---
+++ app/views/formdata/SearchFormData.scala
@@ -0,0 +1,50 @@
+package views.formdata
+
+import scala.collection.mutable.Buffer
+
+import play.api.data.validation.ValidationError
+
+case class SearchFormData(from: List[String] = Nil, to: List[String] = Nil, start: String = "", end: String = "", travellers: List[String] = Nil) {
+
+  //  def this(name: String, password: String, level: GradeLevel, gpa: GradePointAverage, hobbies: Seq[Hobby], majors: Seq[Major]) = this(
+  //    name = name,
+  //    password = password,
+  //    level = level.name,
+  //    gpa = gpa.name,
+  //    hobbies = hobbies.map(_.name).toList,
+  //    majors = majors.map(_.name).toList)
+
+  def validate(): Seq[ValidationError] = {
+    val errors = Buffer[ValidationError]()
+    //    if (name == null || name.length == 0) {
+    //      errors += new ValidationError("name", "No name was given.")
+    //    }
+    //    if (password == null || password.length == 0) {
+    //      errors += new ValidationError("password", "No password was given.")
+    //    } else if (password.length < 5) {
+    //      errors += new ValidationError("password", "Given password is less than five characters.")
+    //    }
+    //    if (hobbies.size > 0) {
+    //      for (hobby <- hobbies if Hobby.findHobby(hobby) == null) {
+    //        errors += new ValidationError("hobbies", "Unknown hobby: " + hobby + ".")
+    //      }
+    //    }
+    //    if (level == null || level.length == 0) {
+    //      errors += new ValidationError("level", "No grade level was given.")
+    //    } else if (GradeLevel.findLevel(level) == null) {
+    //      errors += new ValidationError("level", "Invalid grade level: " + level + ".")
+    //    }
+    //    if (gpa == null || gpa.length == 0) {
+    //      errors += new ValidationError("gpa", "No gpa was given.")
+    //    } else if (GradePointAverage.findGPA(gpa) == null) {
+    //      errors += new ValidationError("gpa", "Invalid GPA: " + gpa + ".")
+    //    }
+    //    if (majors.size > 0) {
+    //      for (major <- majors if Major.findMajor(major) == null) {
+    //        errors += new ValidationError("majors", "Unknown Major: " + major + ".")
+    //      }
+    //    }
+    //    if (errors.size > 0) return errors
+    null
+  }
+}
